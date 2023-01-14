5ff535b285dc10333e3d1a2cca3f335e90721a54
zeroarst <zero.arst@gmail.com>
2023-01-14 22:08:18 +1000

    update test Dependencies

--- src/main/kotlin/io/github/zeroarst/dependencyupdatescommenter/test/Dependency.kt
+++ src/main/kotlin/io/github/zeroarst/dependencyupdatescommenter/test/Dependency.kt
@@ -16,30 +16,33 @@ object JetBrains {
 
 object AndroidX {
 
-    @CommentUpdates
-    val composeCompiler = "androidx.compose.compiler:1.3.2" // "test some text with double quotes"
-
-    @CommentUpdates
-    val composeCompilerInalid = "androidx.lifecycle-viewmodel-compose:2.4.1"
-
-    @CommentUpdates
-    val lifecycleViewModel = "androidx.lifecycle:lifecycle-viewmodel:2.4.1"
-
     // test with commented out annotation
     // available versions:
     // 1.7.3
     // 1.7.2
     // 1.7.1
     // @CheckNewVersions
-    const val core = "androidx.core:core-ktx:1.7.2"
+    const val coreKtx = "androidx.core:core-ktx:1.7.2"
 
     // test with commented out annotation
     // @CheckNewVersions
-    const val core2 = "androidx.core:core-ktx:1.7.2"
+    const val coreAnimation = "androidx.core:core-animation:1.7.2"
+
+    @CommentUpdates
+    val lifecycleViewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1"
+
+    @CommentUpdates
+    val lifecycleViewModelComposeInalid = "androidx.lifecycle-viewmodel-compose:2.4.1"
+
+    @CommentUpdates
+    val lifecycleViewModel = "androidx.lifecycle:lifecycle-viewmodel:2.4.1"
+
+    @CommentUpdates
+    val composeCompiler = "androidx.compose.compiler:1.3.2" // "test some text with double quotes"
 
     // test lazy property.
     @CommentUpdates
-    val core3: String by lazy { "androidx.core:core-ktx:1.7.2" }
+    val composeUI: String by lazy { "androidx.compose.ui:ui:1.2.0" }
 
     // Some similar comments without annotation.
     // available versions:
