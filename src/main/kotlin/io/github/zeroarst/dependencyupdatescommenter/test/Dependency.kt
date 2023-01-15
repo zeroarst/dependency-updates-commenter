d51636588d9282699814f66cd113ebfc1068309c
zeroarst <zero.arst@gmail.com>
2023-01-15 13:35:58 +1000

    update test Dependencies

--- src/main/kotlin/io/github/zeroarst/dependencyupdatescommenter/test/Dependency.kt
+++ src/main/kotlin/io/github/zeroarst/dependencyupdatescommenter/test/Dependency.kt
@@ -1,16 +1,24 @@
 package io.github.zeroarst.dependencyupdatescommenter.test
 
 import io.github.zeroarst.dependencyupdatescommenter.annotaions.CommentUpdates
+
 /**
  * Some docs.
  */
 object Junit {
+
+    // test previous comments.
+
+    // available versions:
+    // 4.13
+    // 4.13.1
+    // 4.13.2
     @CommentUpdates
     val junit = "junit:junit:4.12"
 }
 
 object JetBrains {
-    @CommentUpdates // test some comments here.
+    @CommentUpdates // test some comments after annotation.
     val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.2"
 }
 
@@ -31,6 +39,8 @@ object AndroidX {
     @CommentUpdates
     val lifecycleViewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1"
 
+    // error: invalid dependency.
+    // continue error on second line.
     @CommentUpdates
     val lifecycleViewModelComposeInalid = "androidx.lifecycle-viewmodel-compose:2.4.1"
 
@@ -44,10 +54,20 @@ object AndroidX {
     @CommentUpdates
     val composeUI: String by lazy { "androidx.compose.ui:ui:1.2.0" }
 
-    // test lazy property.
+    // test lazy property with multiple lines.
     @CommentUpdates
     val composeMaterial: String by lazy {
         "androidx.compose.material:material:1.1.1"
+    }
+
+    // test lazy property with multiple lines.
+    @CommentUpdates
+    val composeMaterial3: String by lazy {
+
+
+        "androidx.compose.material:material3:1.1.1"
+
+
     }
 
     // Some similar comments without annotation.
